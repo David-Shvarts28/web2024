53f9c78e54a8ce3234516d7fd64eba82510812c6
David-Shvarts28 <109624143+David-Shvarts28@users.noreply.github.com>
2024-03-05 18:05:56 +0400

    a

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ DB_NAME = 'site'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-settings = {'user_name': 'Вася',
+settings = {"user_name": input()
             }
 
 
@@ -29,66 +29,27 @@ def index():
                            title="Главная страница", user_name=settings.get('user_name', 'Аноним'))
 
 
-@app.route('/test')
-def my_func():
-    return render_template("test.html", title="Тестовая страница", data=['123', '456', 'test connection', '!'])
-
-
-@app.route('/image_sample')
-def image():
-    return f'''<img src="{url_for('static', filename='img/owl.jpeg')}"
-           alt="здесь должна была быть картинка, но не нашлась">'''
-
 
 @app.route('/users_page')
 def return_sample_page():
     db_sess = db_session.create_session()
     users = db_sess.query(User).all()
     print(users)
-    return render_template('users_page.html', title='Список польователей', users=users)
+    return render_template('users_page.html', title='Список пользователей', users=users)
 
 
-@app.route('/avatar', methods=['POST', 'GET'])
-def avatar():
-    if request.method == 'POST':
-        f = request.files['file']
-        settings["avatar_file"] = f.filename
-        f.save(f'static/img/{f.filename}')
-
-    params = {'title': 'Выбор аватара!!!'}
-    if 'avatar_file' in settings:
-        params['avatar'] = url_for('static', filename=f"img/{settings['avatar_file']}")
-    return render_template("avatar.html", **params)
-
-
-class AvatarForm(FlaskForm):
-    file = FileField('Файл wtf', validators=[DataRequired()])
-    submit = SubmitField('Загрузить wtf')
-
-
-@app.route('/avatar2', methods=['POST', 'GET'])
-def avatar2():
-    form = AvatarForm()
-    if form.validate_on_submit():
-        print(form.file.data.filename, secure_filename(form.file.data.filename))
-        avatar_name = secure_filename(form.file.data.filename)
-        settings["avatar_file"] = avatar_name
-        form.file.data.save(f'static/img/{avatar_name}')
-
-    params = {'title': 'Выбор аватара wtf',
-              'form': form}
-    if 'avatar_file' in settings:
-        params['avatar'] = url_for('static', filename=f"img/{settings['avatar_file']}")
-    return render_template("avatar2.html", **params)
-
-
-@app.route('/test_carousel')
+@app.route('/test_carousel', methods=['POST', 'GET'])
 def return_carousel():
     if 'pics' not in settings:
         settings['pics'] = [(f"{url_for('static', filename='img/1.jpeg')}", "first"),
                             (f"{url_for('static', filename='img/2.jpeg')}", "second"),
                             (f"{url_for('static', filename='img/3.jpeg')}", "third")
                             ]
+    if request.method == 'POST':
+        f = request.files['file']
+        settings["avatar_file"] = f.filename
+        f.save(f'static/img/{f.filename}')
+        settings['pics'].append((f"{url_for('static', filename=f'img/{f.filename}')}", "first"))
     return render_template('test_carousel.html', title='Карусель', pics=settings['pics'])
 
 
@@ -125,3 +86,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# http://127.0.0.1:8080/
